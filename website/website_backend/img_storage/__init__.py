7f092fa74cd65bc3d6ed14ff22b1b30197971cb8
ChromatiChord <callumward56@gmail.com>
2021-07-15 23:55:32 +1000

    added barebones swagger documentation

--- website/website_backend/img_storage/__init__.py
+++ website/website_backend/img_storage/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
+# SWAGGER_URL = '/swagger'
+# API_URL = '/static/swagger.json'
+# SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+#     SWAGGER_URL,
+#     API_URL,
+#     config={
+#         'app_name': "Python REST API"
+#     }
+# )
+# app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 from img_storage import routes, models
 
