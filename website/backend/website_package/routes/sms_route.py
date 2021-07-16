18e3ed6b3af770fc61dc435f97caec8e6ff503d2
ChromatiChord <callumward56@gmail.com>
2021-07-17 03:43:48 +1000

    added extra comment

--- website/backend/website_package/routes/sms_route.py
+++ website/backend/website_package/routes/sms_route.py
@@ -7,6 +7,7 @@ class Sms(Resource):
     args = sms_text_args.parse_args()
     # should be set as environment variables, but since python ENV stuff is a nightmare and I can't be bothered
     account_sid = "AC20f57cf543afba11d2b113b94d75c5a4"
+    # sometimes you need to grab a new one from the website
     auth_token  = "930bd3b50be71cdaea20fef820fafcbf"
     client = Client(account_sid, auth_token)
 
