09b0199500fca6428fa4ef0055905b0429aa2a1c
Luke Banicevic <60857954+banaboi@users.noreply.github.com>
2021-07-17 09:48:04 +1000

    Cleaned up front end code

--- website/frontend/frontend_code.js
+++ website/frontend/frontend_code.js
@@ -9,44 +9,21 @@ const dummy_image_data = {
 };
 
 function getImage() {
-    const result = fetch(`${WEBSITE_URL}/image/1`, {
+    const response = fetch(`${WEBSITE_URL}/image/1`, {
         method: "GET",
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
         },
-    })
-        .then((data) => {
-            if (data.status === 200) {
-                data.json().then((result) => {
-                    console.log(result);
-                });
-            }
-        })
-        .catch((error) => {
-            console.log("Error: ", error);
-        });
-}
+    });
 
-function postImage() {
-    const result = fetch(`${WEBSITE_URL}/image/1`, {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(dummy_image_data),
-    })
-        .then((data) => {
-            if (data.status === 200) {
-                data.json().then((result) => {
-                    console.log(result);
-                });
-            }
-        })
-        .catch((error) => {
-            console.log("Error: ", error);
-        });
+    if (!response.ok) {
+        const message = `Error: ${response.status}`;
+        throw new Error(message);
+    } else {
+        const result = await response.json();
+        return result;
+    }
 }
 
 function postImage() {
